2570b3ba13a6e9186fee7afa632c2931a0d38707
Veniamin Krol <kalifriki@gmail.com>
2015-06-29 17:18:13 +0300

    Get rid of redundant comments in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,16 +7,12 @@ module.exports = {
 	module: {
 		loaders: [
 			{ test: /\.html$/, loader: 'html' },
-			//{ test: /knockout-3.3.0\.js/, loader: 'imports?require=>__webpack_require__' },
 			{
       	test: /\.js$/,
       	exclude: /(node_modules|bower_components)/,
       	loader: 'babel'
     	}
-		]/*,
-		noParse: [
-			/knockout-3.3.0\.js/
-    ]*/
+		]
 	},
 	externals: {
 		'ko': 'ko'
